04ea6333afaac387b8a264cb21c867bbce930d18
Jason Tucker <jtgeek@gmail.com>
2015-06-25 02:20:10 -0400

    v0.3
    Still have street vision bug.

--- app/GameBoard/GameBoardCtrl.js
+++ app/GameBoard/GameBoardCtrl.js
@@ -10,11 +10,60 @@ angular.module('myApp.GameBoard', ['ngRoute'])
 }])
 
 .controller('GameBoardCtrl', ['$scope', function($scope) {
-        $scope.map = getMap1();
-        $scope.range = function(n) {
-            return new Array(n);
-        };
-        $scope.toLetter = function(n) {
-            return String.fromCharCode(64 + n);
-        };
+    var map = getMap1();
+    $scope.spy = {title: 'spy', column: map.agentStart.column, row: map.agentStart.row};
+    
+    console.log('Building Hunters');
+    var hunters = [];
+    for (var i = 0; i<4; i++) {
+        var hunter = {column: map.hunterStart.column, row: map.hunterStart.row};
+        hunter.title = i+1;
+        hunters.push(hunter);
+    }
+    $scope.hunters = hunters;
+    
+    console.log('Building Board');
+    var board = new Array(map.height);
+    for(var r = 0; r < map.height; r++) {
+        board[r] = new Array(map.width);
+        for(var c = 0; c < map.width; c++) {
+            var cell = {};
+            cell.isWall = map.walls[r].indexOf(c) >= 0;
+            cell.isStreet = map.streets[r].indexOf(c) >= 0;
+            board[r][c] = cell; 
+        }
+    }
+    $scope.board = board;
+    
+    console.log('Building Functions');
+    $scope.range = function(n) {
+        return new Array(n);
+    };
+    $scope.toLetter = function(n) {
+        return String.fromCharCode(64 + n);
+    };
+        
+    console.log('Building Event Hooks');
+    $scope.startCallback = function(event, ui, item) {
+        console.log("Dragging: " + item);
+        $scope.dragging = item;
+    };
+    
+    $scope.stopCallback = function(event, ui) {
+    };
+     
+     $scope.dropCallback = function(event, ui, column, row) {
+         if ($scope.dragging == 'spy') {
+             $scope.spy.row = row+1;
+             $scope.spy.column = column;
+         }
+         
+         if ($scope.dragging > 0 && $scope.dragging < 5) {
+             $scope.hunters[$scope.dragging-1].row = row+1;
+             $scope.hunters[$scope.dragging-1].column = column;
+         }
+         
+         updateVision($scope.board, $scope.hunters);
+     };
+     
 }]);
--- app/GameBoard/gameBoard.js
+++ app/GameBoard/gameBoard.js
@@ -1,5 +1,57 @@
 /**
  * Created by Jason Tucker on 6/24/2015.
  */
-var gameBoardHeader = $("#gameBoardHeader");
-gameBoardHeader.append("<td></td>");
+'use strict';
+
+function updateVision(board, hunters){
+	$('#gameBoard td').removeClass('vision');
+	$.each(hunters, function(index, hunter){
+		addVision(board, hunter.column, hunter.row, -1, 0);
+		addVision(board, hunter.column, hunter.row, 1, 0);
+		addVision(board, hunter.column, hunter.row, 0, -1);
+		addVision(board, hunter.column, hunter.row, 0, 1);
+	});
+}
+
+function addVision(board, x, y, deltaX, deltaY) {
+	 var height = board.length; 
+	 var width = board[0].length;
+	 var row = $('#gameBoard tr').eq(y);
+	 var column = $(row[0].children[x+1]);
+	 
+	 while(x > 0 && x < width && y > 1 && y < height) {
+		 x += deltaX;
+		 y += deltaY;
+	 	 row = $('#gameBoard tr').eq(y);
+		 column = $(row[0].children[x+1]);
+		 if (!column.hasClass('wall'))	{
+	 		column.addClass('vision');
+			if (column.hasClass('street')) {
+				addStreetVision(x, y, height, width);
+			}
+		 } else {
+			 break;
+		 }
+	 } 
+ }
+ 
+ function addStreetVision(x, y, height, width) {
+	 var directions = [{x:0,y:-1},{x:0,y:1},{x:-1,y:0},{x:1,y:0}];
+	 
+	 $.each(directions, function(index, direction){
+		var tempX = x + direction.x;
+		var tempY = y + direction.y;
+		
+		if (tempX >= 0 && tempX < width && tempY > 0 && tempY < height){
+			var row = $('#gameBoard tr').eq(tempY);
+			var column = $(row[0].children[tempX+1]);
+			
+			if (column.hasClass('street')) {
+				column.addClass('vision');
+			}		
+		}
+	 });
+	 
+	 
+	 
+ }
